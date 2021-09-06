79020bcbbf7e6e6732c4d4c09e09fe3bd7f8eb31
Janick van Dijk <90195082+Janickvd@users.noreply.github.com>
2021-09-06 15:10:46 +0200

    Opdracht #2
    Forum, met informatie ophalen en plaatsen.

---
+++ Wie-ben-jij--45a39d3a-fbdd16c5/forum_registratie.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="registratie_handler.php" method="POST">
+        <input type="text" name="username" id="username" />
+        <label for="usernaam">Username<br></label>
+        <input type="text" name="email" id="email" />
+        <label for="email">Email<br></label>
+        <input type="number" name="age" id="age" />
+        <label for="age">Age<br></label>
+        <input type="submit" name="submit" id="submit" />
+    </form>
+</body>
+</html>
